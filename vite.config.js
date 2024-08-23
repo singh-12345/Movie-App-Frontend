7df7625664568e5a4ec6d4cf0012b648c65865f7
singh- 12345 <amansinghania16@gmail.com>
2024-08-23 18:58:07 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vue from '@vitejs/plugin-vue'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),vue()],
+  plugins: [react()],
 
   server: {
     proxy: {
@@ -13,9 +14,7 @@ export default defineConfig({
       "/uploads/": "http://localhost:3000",
       
     },
-    css: {
-      postcss: './postcss.config.js',
-    },
+  
 
   },
 
