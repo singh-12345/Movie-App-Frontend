cdcf3d31e3051b3f562638faa3e4a3ea9f3966f5
singh- 12345 <amansinghania16@gmail.com>
2024-08-23 22:50:09 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server: {
     proxy: {
-      "/api/": "https://movies-app-zu13.onrender.com",
-
-      "/uploads/": "https://movies-app-zu13.onrender.com",
-      
+      // Proxy for API requests
+      '/api': {
+        target: 'https://movies-app-zu13.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      // Proxy for file uploads
+      '/uploads': {
+        target: 'https://movies-app-zu13.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/uploads/, ''),
+      },
     },
-  
-
   },
-
 });
