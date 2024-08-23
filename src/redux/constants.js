da37757f76f8d0a169f0c5734a0706995915383f
singh- 12345 <amansinghania16@gmail.com>
2024-08-23 22:42:50 +0530

    jeet

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,10 +1,10 @@
 
-export const BASE_URL = "";
+export const BASE_URL = "https://movies-app-zu13.onrender.com";
 
-export const USERS_URL = "/api/v1/users";
+export const USERS_URL = BASE_URL + "/api/v1/users";
 
-export const GENRE_URL = "/api/v1/genre";
+export const GENRE_URL = BASE_URL + "/api/v1/genre";
 
-export const MOVIE_URL = '/api/v1/movies';
+export const MOVIE_URL = BASE_URL + '/api/v1/movies';
 
-export const UPLOAD_URL = '/api/v1/movies';
+export const UPLOAD_URL = BASE_URL + '/api/v1/movies';
