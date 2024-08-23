bea579b6af871a66d076c542655366123be82ea3
singh- 12345 <amansinghania16@gmail.com>
2024-08-23 19:57:28 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
 
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
+      "/api/": "https://movies-app-1-dv39.onrender.com",
 
-      "/uploads/": "http://localhost:3000",
+      "/uploads/": "https://movies-app-1-dv39.onrender.com",
       
     },
   
