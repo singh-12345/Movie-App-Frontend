e2f2ed2c76cd62816300859b371a5cac5f3b405f
singh- 12345 <amansinghania16@gmail.com>
2024-08-23 18:30:33 +0530

    first commit

---
+++ src/redux/constants.js
@@ -0,0 +1,10 @@
+
+export const BASE_URL = "https://movies-app-4xhm.onrender.com";
+
+export const USERS_URL = BASE_URL + "/api/v1/users";
+
+export const GENRE_URL = BASE_URL + "/api/v1/genre";
+
+export const MOVIE_URL = BASE_URL + '/api/v1/movies';
+
+export const UPLOAD_URL = BASE_URL + '/api/v1/movies';
